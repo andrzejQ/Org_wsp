e1d6ec1aaafa7cdba5123a2bc16043daa05ffc0b
andrzejQ <kubaszek@prz.edu.pl>
2021-03-30 18:17:30 +0200

    _r

--- r.cmd
+++
@@ -1,4 +0,0 @@
-:: ostatni człon aktulanego foldera %cd% poprzedzony http://localhost/ oraz wywołanie przeglądarki
-@FOR /F "delims=|" %%i IN ("%cd%") DO start "jekyll-localhost" http://localhost:4000/%%~ni/
-::uruchomienie lokalnego serwera www
-bundle exec jekyll serve
